a2ec383f8a3146faf569c61637beab29e8fcdb16
lsh111112 <lsh111112@naver.com>
2016-11-21 19:39:55 +0900

    initial

--- app/src/main/java/com/example/sunwo/money_book/ChartView.java
+++ app/src/main/java/com/example/sunwo/money_book/ChartView.java
@@ -77,6 +77,7 @@ public class ChartView extends View {
         CategorySeries series = new CategorySeries("계열 타이틀");
         DefaultRenderer renderer = new DefaultRenderer();
 //asdasdadasdasdasd
+        //asdfasdfasdf
         renderer.setShowLabels(true);
         renderer.setLegendTextSize(30); //凡例の文字サイズ
         renderer.setShowLegend(false);
